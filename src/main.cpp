f8f839e2fbda9353a2c01cc37c6ef768f84f89e4
Eric LING <eriling@e3r2p6.clusters.42paris.fr>
2022-06-15 10:07:38 +0200

    clean

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,55 @@
 #define LIMIT_PORT 65535
 
 
+void signal_handler (int n);
+bool check_input(int ac, char **av);
+
+int main(int ac, char **av){
+
+    if (check_input(ac, av) == false)
+        return (0);
+    Socket sk("127.0.0.1", atoi(av[1]));
+    Server sv;
+    try {
+        signal(SIGINT, signal_handler);
+        sk.CreateFd(AF_INET, SOCK_STREAM, 0);
+        sk.SetAddr(AF_INET);
+        sk.Bind();
+        sv.Listen(&sk, 10);
+        while(1){
+            FD_ZERO(&sk._readfs);
+            // set master fd in fd_set
+            FD_SET(sk._fd, &sk._readfs);
+            sk._max_fd = sk._fd;
+            // set client fd in fd_set
+            for(size_t i = 0; i < sk._client.size(); i++){
+                FD_SET(sk._client[i], &sk._readfs);
+                if (sk._client[i] > sk._max_fd)
+                    sk._max_fd = sk._client[i];
+            }
+            //waiting for a new client
+            sv.Select(&sk, 0);
+            //connexion client
+            if (FD_ISSET(sk._fd, &sk._readfs))
+                sv.Accept(&sk);
+
+            //check clients activity
+            for (size_t i = 0; i < sk._client.size(); i++){
+                if (FD_ISSET(sk._client[i], &sk._readfs)){
+                    sv.Recv(&sk, i, 0);
+                    memset(sv._buff, 0, LIMIT_MSG);
+                }
+            }
+        }
+    }
+    catch (std::exception &e){
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+    return (0);
+}
+
+/* Utils */
+
 void signal_handler (int n){
     std::cerr<<std::endl;
     std::cerr << "Interruption by signal " << n << std::endl;
@@ -37,49 +86,4 @@ bool check_input(int ac, char **av){
     else if (n < 0 || n > LIMIT_PORT)
         return (str_error(0, "the highest TCP port number is 65 535\n"));
     return (true);
-}
-
-int main(int ac, char **av){
-
-    if (check_input(ac, av) == false)
-        return (0);
-    Socket sk("127.0.0.1", atoi(av[1]));
-    Server sv;
-    try {
-        signal(SIGINT, signal_handler);
-        sk.CreateFd(AF_INET, SOCK_STREAM, 0);
-        sk.SetAddr(AF_INET);
-        sk.Bind();
-        sv.Listen(&sk, 10);
-        while(1){
-            FD_ZERO(&sk._readfs);
-            // set master fd in fd_set
-            FD_SET(sk._fd, &sk._readfs);
-            sk._max_fd = sk._fd;
-            // set client fd in fd_set
-            for(size_t i = 0; i < sk._client.size(); i++){
-                FD_SET(sk._client[i], &sk._readfs);
-                if (sk._client[i] > sk._max_fd)
-                    sk._max_fd = sk._client[i];
-            }
-
-            //waiting for a new client
-            sv.Select(&sk, 0);
-            //connexion client
-            if (FD_ISSET(sk._fd, &sk._readfs))
-                sv.Accept(&sk);
-
-            //check clients activity
-            for (size_t i = 0; i < sk._client.size(); i++){
-                if (FD_ISSET(sk._client[i], &sk._readfs)){
-                    sv.Recv(&sk, i, 0);
-                    memset(sv._buff, 0, LIMIT_MSG);
-                }
-            }
-        }
-    }
-    catch (std::exception &e){
-        std::cerr << "Error: " << e.what() << std::endl;
-    }
-    return (0);
 }
